80ab254301173d9bf6fb1c46486451ce1e6d764e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-09-02 01:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getIssues() {
-  const repo = 'jshwa/javascript-fetch-lab'
+  const repo = 'ayesha-x-ansari/js-templates-template-engines-readme-v-000'
+
   fetch(`https://api.github.com/repos/${repo}/issues`, {
     method: 'GET',
     headers: {
